30cf2ea5b96b1efa97824fc117d65b2ce808bfb2
ditodileep050 <dd9834@srmist.edu.in>
2026-02-25 08:49:49 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,14 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Palindrome Checker App     ");
+        System.out.println("           Version 1.0           ");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("=================================");
+
+    }
+}
